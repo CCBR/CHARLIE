12738a3c7ba75af3d105b94a29a8697be707af3e
Vishal N Koparde <vishal.koparde@nih.gov>
2021-02-26 10:51:06 -0500

    quant annotation bug fix...need to index before joining

--- scripts/annotate_clear_quant.py
+++ scripts/annotate_clear_quant.py
@@ -16,6 +16,7 @@ annotations.set_index(["hg38ID"],inplace=True)
 quantfile=sys.argv[2]
 quant=pandas.read_csv(quantfile,sep="\t",header=None,names=["chrom","start","end","name","score","quant_strand","thickStart","thickEnd","itemRgb","exonCount","exonSizes","exonOffsets","readNumber","circType","geneName","isoformName","index","flankIntron","FPBcirc","FPBlinear","CIRCscore"])
 quant["hg38ID"]=quant.apply(lambda row: row.chrom+":"+str(row.start)+"-"+str(row.end),axis=1)
+quant.set_index(["hg38ID"],inplace=True)
 
 x=quant.join(annotations)
 x.to_csv(quantfile+'.annotated',sep="\t",header=True)
