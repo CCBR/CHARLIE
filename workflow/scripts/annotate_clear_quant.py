49d883ddcaf34b701d6cc1dee04cff2056317b79
kopardev <vishal.koparde@nih.gov>
2022-05-20 11:58:19 -0400

    adding "quant_" prefix to avoid column name clashes in annotate_clear_quant.py

--- workflow/scripts/annotate_clear_quant.py
+++ workflow/scripts/annotate_clear_quant.py
@@ -1,5 +1,5 @@
 # AUTHOR: Vishal N Koparde, Ph.D, CCBR, NCI
-# This script annotates quant.txt file from CLEAR workflow with hg38_2_hg19_lookup.txt
+# This script annotates quant_txt file from CLEAR workflow with hg38_2_hg19_lookup.txt
 
 import pandas
 import re
@@ -16,8 +16,8 @@ annotations=pandas.read_csv(lookupfile,sep="\t",header=0)
 annotations.set_index([indexcol],inplace=True)
 
 quantfile=sys.argv[2]
-quant=pandas.read_csv(quantfile,sep="\t",header=None,names=["chrom","start","end","name","score","quant_strand","thickStart","thickEnd","itemRgb","exonCount","exonSizes","exonOffsets","readNumber","circType","geneName","isoformName","index","flankIntron","FPBcirc","FPBlinear","CIRCscore"])
-quant[indexcol]=quant.apply(lambda row: row.chrom+":"+str(row.start)+"-"+str(row.end),axis=1)
+quant=pandas.read_csv(quantfile,sep="\t",header=None,names=["quant_chrom","quant_start","quant_end","quant_name","quant_score","quant_quant_strand","quant_thickStart","quant_thickEnd","quant_itemRgb","quant_exonCount","quant_exonSizes","quant_exonOffsets","quant_readNumber","quant_circType","quant_geneName","quant_isoformName","quant_index","quant_flankIntron","quant_FPBcirc","quant_FPBlinear","quant_CIRCscore"])
+quant[indexcol]=quant.apply(lambda row: row.quant_chrom+":"+str(row.quant_start)+"-"+str(row.quant_end),axis=1)
 quant.set_index([indexcol],inplace=True)
 
 x=quant.join(annotations)
