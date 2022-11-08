39672729434f19aa76514703b0eb31cd1f8b853c
kopardev <vishal.koparde@nih.gov>
2022-11-08 10:32:03 -0500

    if outdir exists ... delete and recreate it

--- workflow/scripts/_multifasta2separatefastas.sh
+++ workflow/scripts/_multifasta2separatefastas.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# absolute path of input FASTA
 fasta=$1
+# absolute path of output folder
 outdir=$2
+if [ -d $outdir ];then rm -rf $outdir;fi
 mkdir -p $outdir && cd $outdir
 cat $fasta | awk '{
-        if (substr($0, 1, 1)==">") {filename=(substr($0,2) ".fasta")}
+        if (substr($0, 1, 1)==">") {filename=(substr($0,2) ".fa")}
                         print $0 >> filename
                                 close(filename)
                         }'
