c745698c2cef791d6134fb4eaa8c85247ed3b7c8
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-05-16 16:59:13 -0400

    feat: add version function

--- src/charlie/util.py
+++ src/charlie/util.py
@@ -6,3 +6,9 @@ def smk_base(rel_path):
         os.path.split(os.path.dirname(os.path.realpath(__file__)))[0]
     )[0]
     return os.path.join(basedir, rel_path)
+
+
+def get_version():
+    with open(smk_base("VERSION"), "r") as f:
+        version = f.readline().strip()
+    return version
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,12 +1,22 @@
 import os
+import re
 import sys
 
 SCRIPT_DIR = os.path.join(
     os.path.split(os.path.dirname(os.path.abspath(__file__)))[0], "src", "charlie"
 )
 sys.path.append(SCRIPT_DIR)
-from util import smk_base
+from util import *
 
 
 def test_smk_base():
     assert smk_base("").endswith("charlie/")
+
+
+def test_semantic_version():
+    assert bool(
+        re.match(
+            r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$",
+            get_version(),
+        )
+    )
